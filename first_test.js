fbf6cf67eb96ea05f716fedefb6d4411dad43e58
hideshis <hideshis1990@gmail.com>
2021-12-18 21:42:48 +0900

    テストシナリオを追加

--- first_test.js
+++ first_test.js
@@ -8,3 +8,8 @@ Scenario('test something', ({ I }) => {
     I.see('Write a guide', '.todo-list');
     I.see('1 item left', '.todo-count');
 });
+
+Scenario('test codeceptjs@github', ({ I }) => {
+    I.amOnPage('https://github.com/codeceptjs/CodeceptJS');
+    I.see('CodeceptJS', '#repository-container-header > div.d-flex.mb-3.px-3.px-md-4.px-lg-5 > div > h1 > strong > a');
+});
